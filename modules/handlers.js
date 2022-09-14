577081d79214061cc3e30a040b289b242d04b136
Monika <monidavies@gmail.com>
2022-09-13 21:13:09 -0700

    Error handling

--- modules/handlers.js
+++ modules/handlers.js
@@ -19,13 +19,12 @@ Handler.getCity = (req, res) => {
     local_purchasing_pwr_idx: '',
     gas_price: '',
     search_city: ''
-  }
-  //const url = `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_KEY}&q=${req.query.city}&format=json`;
+  };
+
   const config = {
     url: `https://us1.locationiq.com/v1/search.php?key=${process.env.LOCATION_KEY}&q=${req.query.city}&format=json`,
     headers: { 'referer': 'http://localhost:3001/' }
   };
-
   axios(config)
     .then(response => {
       console.log('Location Response: ', response.data[0]);
@@ -37,37 +36,52 @@ Handler.getCity = (req, res) => {
       resultObj['search_city'] = response.data[0].display_name.split(',')[0];
       getGasPrices(locationObj)
         .then(result => {
-          resultObj['gas_price'] = result.data[0].Price;
+          console.log('Gas Price Promise:', result.data[0]);
+          result.data[0] ? resultObj['gas_price'] = result.data[0].Price : resultObj['gas_price'] = 'Gas prices not available.';
           console.log('Dataset: ', Dataset);
-          console.log('resultObj Object: ', {city: resultObj.search_city});
-          Dataset.find({ city: { $regex: `(?i)${resultObj.search_city}(?-i)` } })
-            .then(response => {
-              resultObj.city = response[0].city;
-              resultObj.col_idx = response[0].col_idx;
-              resultObj.rent_idx = response[0].rent_idx;
-              resultObj.col_plus_rent_idx = response[0].col_plus_rent_idx;
-              resultObj.groceries_idx = response[0].groceries_idx;
-              resultObj.restaurant_idx = response[0].restaurant_idx;
-              resultObj.local_purchasing_pwr_idx = response[0].local_purchasing_pwr_idx;
-              res.send(resultObj);
-            });
-          
+          console.log('resultObj Object: ', { city: resultObj.search_city });
+
+          try {
+            Dataset.find({ city: { $regex: `(?i)${resultObj.search_city}(?-i)` } })
+              .then(response => {
+                if (response.length > 0) {
+                  resultObj.city = response[0].city;
+                  resultObj.col_idx = response[0].col_idx;
+                  resultObj.rent_idx = response[0].rent_idx;
+                  resultObj.col_plus_rent_idx = response[0].col_plus_rent_idx;
+                  resultObj.groceries_idx = response[0].groceries_idx;
+                  resultObj.restaurant_idx = response[0].restaurant_idx;
+                  resultObj.local_purchasing_pwr_idx = response[0].local_purchasing_pwr_idx;
+                  res.send(resultObj);
+                }
+                else {
+                  res.send('City data not found.');
+                }
+              });
+          } catch (error) {
+            console.log('Database query error:', error);
+          }
         });
     })
     .catch(error => {
       console.log(error);
+      res.send(error);
     });
 };
 
 const getGasPrices = (locationObj) => {
-  let isUSA = false;
+  try {
+    let isUSA = false;
 
-  if (locationObj.country === 'USA') {
-    isUSA = true;
+    if (locationObj.country === 'USA') {
+      isUSA = true;
+    }
+    const url = `https://www.gasbuddy.com/gaspricemap/county?lat=${locationObj.lat}&lng=${locationObj.lon}&usa=${isUSA}`;
+    console.log(url);
+    return axios.post(url);
+  } catch (error) {
+    console.log('the catch has been triggered! Error: ', error);
   }
-  const url = `https://www.gasbuddy.com/gaspricemap/county?lat=${locationObj.lat}&lng=${locationObj.lon}&usa=${isUSA}`;
-  console.log(url);
-  return axios.post(url);
 };
 
 Handler.savedResults = async (req, res, next) => {
